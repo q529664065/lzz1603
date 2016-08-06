5f178f9f253fc99e3c532c266d058f0131a3dd21
千锋 <qianfeng@liuzhi.local>
2016-08-06 15:24:04 +0800

    新增加了roottableView

---
+++ 新工程/QQ/RootTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  RootTableViewCell.h
+//  QQ
+//
+//  Created by 千锋 on 16/8/6.
+//  Copyright © 2016年 刘硕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootTableViewCell : UITableViewCell
+
+@end
---
+++ 新工程/QQ/RootTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  RootTableViewCell.m
+//  QQ
+//
+//  Created by 千锋 on 16/8/6.
+//  Copyright © 2016年 刘硕. All rights reserved.
+//
+
+#import "RootTableViewCell.h"
+
+@implementation RootTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
